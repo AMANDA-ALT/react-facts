0c4252b39090bf1326dbd9561c46ad7caed1d04f
Amanda <amandabaratadev@gmail.com>
2023-02-15 20:06:46 +0000

    first project

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <main className='Main'>
+            <h1 className='Main-title'> Fun facts about React</h1>
+            <ul className='Main-facts'>
+                <li>Was firts released by 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is mantained by Facebook</li>
+                <li>Power thousands of enterprise apps, including mobile apps!</li>
+            </ul>
+        </main>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='Nav'>
+            <img src='./image/logo-react.png'/>
+
+            <h3>React Facts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default Navbar
